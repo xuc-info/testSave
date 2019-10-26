ffb1fb9733be0d870a02c0f0befd867e8a91fcd4
xuc-info <2670234100@qq.com>
2019-10-26 21:54:36 +0800

    mmmmmmmm

--- simpletest/src/com/xuc/test/TestJavaSE.java
+++ simpletest/src/com/xuc/test/TestJavaSE.java
@@ -7,9 +7,10 @@ public class TestJavaSE {
 	public void test1() {
 
 		int[] arr = { 4, 1, 6, 3, 9, 8 };
-		
-		System.out.println("=====what=====");
 
+		System.out.println("=====what=====");
+		System.out.println("othsre   say");
+		
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length; j++) {
 				if (arr[i] < arr[j]) {
