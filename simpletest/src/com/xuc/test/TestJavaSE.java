0c7c2cb6e13d8faa7f5a014dea6e8b833d7db259
xuc-info <2670234100@qq.com>
2019-10-26 16:20:13 +0800

    comit all

---
+++ simpletest/src/com/xuc/test/TestJavaSE.java
@@ -0,0 +1,41 @@
+package com.xuc.test;
+
+import org.junit.Test;
+
+public class TestJavaSE {
+	@Test
+	public void test1() {
+	
+		int[] arr = { 4, 1, 6, 3, 9, 8 }; // 定义一个数组
+		
+		/*for (int i = 0; i < arr.length-1; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[i]<arr[j]) {
+					int temp=arr[i];
+					
+					arr[i]=arr[j];
+					arr[j]=temp;
+					
+				}
+				
+			}
+		}*/
+		
+
+		for (int i = 0; i < arr.length-1; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[i]<arr[j]) {
+					int temp=arr[i];
+					
+					arr[i]=arr[j];
+					arr[j]=temp;
+					
+				}
+				
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
---
+++ simpletest/src/com/xuc/test/TestValue.java
@@ -0,0 +1,23 @@
+package com.xuc.test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class TestValue {
+	public static void main(String[] args) {
+		System.getProperty("java.class.path");
+		System.out.println(13&11);
+	}
+}
