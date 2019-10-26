2a9e23061d70b9f0c9f646005691fea6693fba58
xuc-info <2670234100@qq.com>
2019-10-26 21:41:21 +0800

    dada

--- simpletest/src/com/xuc/test/TestJavaSE.java
+++ simpletest/src/com/xuc/test/TestJavaSE.java
@@ -8,7 +8,7 @@ public class TestJavaSE {
 
 		int[] arr = { 4, 1, 6, 3, 9, 8 };
 		
-		System.out.println("=======what=====");
+		System.out.println("=====what=====");
 
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length; j++) {
